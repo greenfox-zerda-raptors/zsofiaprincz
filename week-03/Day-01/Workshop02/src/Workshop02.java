ecaac8d3bbfc3bcda7d7d7e32bcd0de3248dbea2
ZSOFI PRINCZ <zsofiaprincz@gmail.com>
2016-11-02 14:29:02 +0100

    Workshop02 finished

---
+++ week-03/Day-01/Workshop02/src/Workshop02.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zsofiaprincz on 02/11/16.
+ */
+public class Workshop02 {
+    public static void main(String[] args)  {
+        int b = 100;
+
+        System.out.println(b-7);
+    }
+
+    }

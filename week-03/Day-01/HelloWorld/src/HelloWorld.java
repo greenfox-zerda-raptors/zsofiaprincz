366e9f34494b668da258c8cfc34908781e7cc90a
ZSOFI PRINCZ <zsofiaprincz@gmail.com>
2016-11-02 14:48:16 +0100

    week03 day01 filestructure and 1-3 workshops

---
+++ week-03/Day-01/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by zsofiaprincz on 02/11/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello Matey");
+
+
+    }
+}
---
+++ week-03/Day-01/Workshop01/src/Workshop01.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zsofiaprincz on 02/11/16.
+ */
+public class Workshop01{
+    public static void main(String[] args) {
+        int a = 3;
+        // make it bigger by 10
+
+        System.out.println(a+10);
+    }
+}

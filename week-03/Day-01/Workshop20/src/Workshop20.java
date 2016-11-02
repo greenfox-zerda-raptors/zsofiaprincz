450d738e76c210684686e90c99048d0be8dedb60
ZSOFI PRINCZ <zsofiaprincz@gmail.com>
2016-11-02 18:58:39 +0100

    finished all workshop exercises

---
+++ week-03/Day-01/Workshop20/src/Workshop20.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zsofiaprincz on 02/11/16.
+ */
+public class Workshop20 {
+    public static void main(String... args){
+        String specialCharacters = "We're here at GreenFox's office and I'm a member of the \"Raptors\" team." ;
+        // Create a string variable with the following value:
+        // We're here at GreenFox's office and I'm a member of the "Raptors" team.
+        // (with the quotation marks ;) )
+
+        System.out.println(specialCharacters);
+    }
+}
---
+++ week-03/Day-01/Workshop21/src/Workshop21.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zsofiaprincz on 02/11/16.
+ */
+public class Workshop21 {
+    public static void main(String... args){
+        String proverb = "If it ain't broke, don't fix it.";
+        // Print the "don't fix it" part using the proverb variable
+
+        System.out.println(proverb.substring(proverb.indexOf("d"), (proverb.length()-1)));
+    }
+}

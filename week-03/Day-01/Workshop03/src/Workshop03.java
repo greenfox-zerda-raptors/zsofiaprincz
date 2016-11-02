2c8a4c7fd81736012eca108ed9d4b03631c13e1c
ZSOFI PRINCZ <zsofiaprincz@gmail.com>
2016-11-02 14:51:56 +0100

    workshop03

---
+++ week-03/Day-01/Workshop03/src/Workshop03.java
@@ -0,0 +1,12 @@
+/**
+ * Created by zsofiaprincz on 02/11/16.
+ */
+public class Workshop03 {
+    public static void main(String[] args) {
+        int c = 44;
+        // please double c's value
+
+        System.out.println(c*2);
+    }
+}
+

8117e223e9cb1b3350497673f1cca7439481ed6e
ZSOFI PRINCZ <zsofiaprincz@gmail.com>
2016-11-03 13:17:40 +0100

    Day 2 file structure and workshop 22

--- week-03/Day-02/src/Workshop23.java
+++ week-03/Day-02/src/Workshop23.java
@@ -2,4 +2,19 @@
  * Created by zsofiaprincz on 03/11/16.
  */
 public class Workshop23 {
+    public static void main(String[] args) {
+        int v = 426;
+        // print 'Yeah!' if dividable by 4 but print 'End of program' after regardless
+        boolean examination = 426 % 4 == 0;
+        String answer = "";
+        if (examination=true) {
+            answer = "Yeah!";
+        } else {
+            answer = "End of program";
+        }
+
+        {
+            System.out.println(answer);
+        }
+    }
 }

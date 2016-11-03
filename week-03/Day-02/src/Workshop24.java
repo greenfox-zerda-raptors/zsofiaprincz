e9ddd842af43404dd2f8f91bf1e6c7dab3307d93
ZSOFI PRINCZ <zsofiaprincz@gmail.com>
2016-11-03 13:35:09 +0100

    Day 2 file structure and workshop 22

---
+++ week-03/Day-02/src/Workshop24.java
@@ -0,0 +1,18 @@
+/**
+ * Created by zsofiaprincz on 03/11/16.
+ */
+public class Workshop24 {
+    public static void main(String[] args) {
+        int w = 24;
+        int out = 0;
+        // if w is even increment out by one
+
+        boolean wIsEven = w % 2 == 0;
+        if (wIsEven ) {
+            out++;
+        }
+        System.out.println(out);
+
+    }
+
+}
---
+++ week-03/Day-02/src/Workshop25.java
@@ -0,0 +1,14 @@
+/**
+ * Created by zsofiaprincz on 03/11/16.
+ */
+public class Workshop25 {
+    public static void main(String[] args) {
+        String x = "cheese";
+        // Tell if the x has even or odd number of
+        // characters with a True for even and
+        // false False output otherwise
+
+        System.out.println();
+    }
+
+}

8854455122a6bdb4d954027e3d04e40b5850a476
ZSOFI PRINCZ <zsofiaprincz@gmail.com>
2016-11-25 15:41:10 +0100

    some shitty solution

--- week-06/Project/src/ImageGallery.java
+++ week-06/Project/src/ImageGallery.java
@@ -6,29 +6,35 @@ import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by zsofiaprincz on 25/11/16.
  */
 public class ImageGallery extends JFrame {
 
-    Image image1;
-    Image image2;
-    Image image3;
-    Image image4;
-    Image image5;
-    int pos =0;
-
-    JLabel imageLabel;
-
-    ArrayList<Image> notSureINeedThis ;
-    JLabel image1Name;
-    JLabel image2Name;
-    JLabel image3Name;
-    JLabel image4Name;
-    JLabel image5Name;
+    ImageIcon image1;
+    ImageIcon image2;
+    ImageIcon image3;
+    ImageIcon image4;
+    ImageIcon image5;
 
 
+    JLabel imageLabel1;
+    JLabel imageLabel2;
+    JLabel imageLabel3;
+    JLabel imageLabel4;
+    JLabel imageLabel5;
+
+
+
+    ArrayList<JLabel> myImages = new ArrayList<JLabel>();
+    int index;
+
+    ArrayList<JLabel> imageNames = new ArrayList<JLabel>();
+    int nameindex;
+
+    JPanel wrapperPanel;
     JPanel topPanel;
     JPanel bottomPanel;
 
@@ -47,104 +53,121 @@ public class ImageGallery extends JFrame {
     }
 
     public ImageGallery() {
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Favourite paintings of Caravaggio");
+        setVisible(true);
 
 
         Toolkit tk = Toolkit.getDefaultToolkit();
         Dimension dim = tk.getScreenSize();
 
-        setSize(600, 600);
+        setLayout(new FlowLayout());
+        setSize(1000, 1000);
         int xPos = (dim.width / 2) - (getWidth() / 2);
         int yPos = (dim.height / 2) - (getHeight() / 2);
         setLocation(xPos, yPos);
-        setResizable(false);
+        setResizable(true);
         setTitle("Favourite paintings of Caravaggio");
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 
-        topPanel = new JPanel(){
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(image1, 0, 0, null);
-            }
-        };
 
+
+        topPanel = new JPanel();
         add(topPanel);
+//        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
 
-        topNext = new JButton(">");
-        topPanel.add(topNext);
-        topNext.addActionListener(new ActionListener() {
+        topPrevious = new JButton("<");
+        topPrevious.setBorderPainted(false);
+        topPrevious.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                pos = pos + 1;
-                if (pos >= getImages().length) {
-                    pos = getImages().length - 1;
-                }
-                showImage(pos);
+                index--;
+                if (index < 0) {
+                    index = myImages.size() - 1;}
+
+                imageLabel1.setIcon(myImages.get(index).getIcon());
+//                imageLabel1.setPreferredSize(new Dimension(100,100));
+
+
+
 
             }
         });
 
-        topPrevious = new JButton("<");
-        topPrevious.setHorizontalAlignment(SwingConstants.RIGHT);
-
-
-
         topPanel.add(topPrevious);
-        topPrevious.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+
+        image1 = new ImageIcon("image1.jpg");
 
 
-        image1 = Toolkit.getDefaultToolkit().getImage("image1.jpg");
-        image2 = Toolkit.getDefaultToolkit().createImage("image2.jpg");
-        image3 = Toolkit.getDefaultToolkit().createImage("image3.jpg");
-        image4 = Toolkit.getDefaultToolkit().createImage("image4.jpg");
-        image5 = Toolkit.getDefaultToolkit().createImage("image5.jpg");
+        imageLabel1 = new JLabel(image1);
+        topPanel.add(imageLabel1);
+
+
+        topNext = new JButton(">");
+        topNext.setBorderPainted(false);
+        topNext.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                index++;
+                if(index == myImages.size()) {
+                    index = 0;}
+                imageLabel1.setIcon(myImages.get(index).getIcon());
+//                imageLabel1.setPreferredSize(new Dimension(100,100));
+            }
+        });
+        topPanel.add(topNext);
+
+
+        bottomPanel = new JPanel();
+        this.add(bottomPanel);
+//        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
+
+
+        image2 = new ImageIcon("image2.jpg");
+        imageLabel2 = new JLabel(image2);
+        imageLabel2.setPreferredSize(new Dimension(100,100));
+        bottomPanel.add(imageLabel2);
+
+        image3 = new ImageIcon("image3.jpg");
+        imageLabel3 = new JLabel(image3);
+        imageLabel3.setPreferredSize(new Dimension(100,100));
+        bottomPanel.add(imageLabel3);
+
+        image4 = new ImageIcon("image4.jpg");
+        imageLabel4 = new JLabel(image4);
+        imageLabel4.setPreferredSize(new Dimension(100,100));
+        bottomPanel.add(imageLabel4);
+
+        image5 = new ImageIcon("image5.jpg");
+        imageLabel5 = new JLabel(image5);
+        imageLabel5.setPreferredSize(new Dimension(100,100));
+        bottomPanel.add(imageLabel5);
 
 
 
+        wrapperPanel = new JPanel();
+        wrapperPanel.add(topPanel);
+        wrapperPanel.add(bottomPanel);
+        wrapperPanel.setLayout(new BoxLayout(wrapperPanel, BoxLayout.Y_AXIS));
+        wrapperPanel.setBackground(Color.LIGHT_GRAY);
+        this.add(wrapperPanel);
 
-        imageLabel = new JLabel();
 
+        this.pack();
+        int xPosition = (dim.width / 2) - (this.getWidth() / 2);
+        int yPosition = (dim.height / 2) - (this.getHeight() / 2);
+        this.setLocation(xPosition, yPosition);
 
+        myImages = new ArrayList(Arrays.asList(imageLabel1,imageLabel2,imageLabel3,imageLabel4,imageLabel5));
 
-
-
-
-        ArrayList<Image> imageList = new ArrayList<>();
-        imageList.add(image1);
-        imageList.add(image2);
-        imageList.add(image3);
-        imageList.add(image4);
-        imageList.add(image5);
 
 
 
     }
-        public String[] getImages() {
-
-            File file = new File(getClass().getResource("/Users/zsofiaprincz/Greenfox/zsofiaprincz/week-06/Project").getFile());
-
-            String[] imagesList = file.list();
-
-            return imagesList;
-
-        }
-
-        public void showImage(int index)
-        {
-            String[] imagesList = getImages();
-
-            String imageName = imagesList[index];
-
-            ImageIcon icon = new ImageIcon(getClass().getResource("/Users/zsofiaprincz/Greenfox/zsofiaprincz/week-06/Project"+imageName));
-
-            Image image = icon.getImage().getScaledInstance(imageLabel.getWidth(), imageLabel.getHeight(), Image.SCALE_SMOOTH);
-
-            imageLabel.setIcon(new ImageIcon(image));
-        }
-
 
 
 

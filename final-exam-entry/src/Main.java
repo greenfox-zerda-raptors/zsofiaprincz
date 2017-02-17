15561c6b8ba92269c0adc9a1b2213b0004568feb
ZSOFI PRINCZ <zsofiaprincz@gmail.com>
2017-02-17 12:04:09 +0100

    final exam entry - array shifter with return and print

--- final-exam-entry/src/Main.java
+++ final-exam-entry/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by zsofiaprincz on 17/02/17.
  */
@@ -5,7 +7,10 @@
 //Create a function that takes an array and a number and shifts the values of the array by the given number like: [1, 2, 3, 4, 5] and 2 becomes [4, 5, 1, 2, 3]
 public class Main {
 
-    public void arrayReverser(int original[], int shift) {
+
+
+
+    public static int[] arrayReverser(int original[], int shift) {
         for (int j = 0; j < shift; j++) {
             for (int i = original.length - 1; i > 0; i--) {
                 int temp = original[i];
@@ -14,8 +19,18 @@ public class Main {
 
             }
 
-        }
+        } return original;
     }
+
+
+
+    public static void main(String[] args) {
+
+        int[] test =  new int[] {1,2,3,4,5};
+        int shift = 3;
+        System.out.println(Arrays.toString(arrayReverser(test,shift)));
+    }
+
 }
 
 
